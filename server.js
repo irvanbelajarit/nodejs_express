ad14f8ccfdd0cdf0e06f8a1c4bbbff2f26b12433
irvanln <irvina22@gmail.com>
2020-11-10 18:54:22 +0700

    staticfileserving

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 
+const path = require('path');
+
 const app = express();
 
 const port =3000;
 
+app.use(express.static(path.join(__dirname,'./static')));
+
 app.get('/',(request,response)=>{
 
-    response.send("hello world express");
+    // response.send("hello world express");
+
+    response.sendFile(path.join(__dirname,'./static/index.html'));
 });
 
 app.listen(port,()=>{
