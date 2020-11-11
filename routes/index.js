2281a9ca41667d3058ab3e01171e3543e34e9efe
irvanln <irvina22@gmail.com>
2020-11-11 23:46:54 +0700

    session cookie

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const materiKuliahRoute = require('./materikuliah');
 const feedbackRoute = require('./feedback');
+const { request } = require('express');
 
 const router = express.Router();
 
@@ -10,6 +11,13 @@ module.exports = (params) => {
     // response.send("hello world express");
 
     // response.sendFile(path.join(__dirname, './static/index.html'));
+
+    if (!request.session.visitcount) {
+      request.session.visitcount = 0;
+    }
+
+    request.session.visitcount += 1;
+    console.log(`Jumlah yang Visit : ${request.session.visitcount}`);
 
     response.render('pages/index', { pageTitle: 'web programming' });
   });
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const path = require('path');
+const cookieSession = require('cookie-session');
 
 const FeedbackService = require('./services/FeedbackService');
 
@@ -11,6 +12,15 @@ const routes = require('./routes');
 const app = express();
 
 const port = 3000;
+
+app.set('trust proxy', 1);
+
+app.use(
+  cookieSession({
+    name: 'session',
+    keys: ['irvan', 'webprogramming'],
+  })
+);
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './views'));
